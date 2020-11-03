a56417d1a9a9853346a8a65bae14799461f56a1d
jackie0922youhei <jackie0922youhei@gmail.com>
2020-11-03 07:44:16 +0000

    favorite非同期

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,14 @@ class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.build(book_id: params[:book_id])
     favorite.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @favoriteCounts = Favorite.where(book_id: params[:book_id])
   end
 
   def destroy
     favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @favoriteCounts = Favorite.where(book_id: params[:book_id])
   end
 end
