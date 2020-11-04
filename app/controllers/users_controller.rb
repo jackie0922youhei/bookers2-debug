d2ab3c934b3c9592da88fa03bbf4e7052b298129
jackie0922youhei <jackie0922youhei@gmail.com>
2020-11-04 08:43:57 +0000

    応用課題7終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,17 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @users = user.followers
   end
+  
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+  
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+  
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
 
   private
   def user_params
