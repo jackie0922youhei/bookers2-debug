44562c751ded21058c23e4c5652b8ad72c3f67a6
jackie0922youhei <jackie0922youhei@gmail.com>
2020-11-07 03:00:17 +0000

    chats controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,7 +1,40 @@
 class ChatsController < ApplicationController
+  
+  before_action :set_room
+  before_action :set_chat, only: [:show]
+  
   def create
+    UserRoom.where(user_id: current_user.id, room_id: @room.id)
+    @chat = Chat.create(chat_params)
+    if @chat.save
+      @chat = Chat.new
+      gets_entries_all_messages
+    end
   end
 
   def show
+    UserRoom.where(user_id: current_user.id, room_id: @room.id).present?
+    @chats = @room.chats.includes(:user).order("created_at asc")
+    @chat = Chat.new
+    @entries = @room.user_rooms
   end
+  
+  private
+  def set_room
+    @room = Room.find(params[:chat][room_id])
+  end
+  
+  def set_chat
+    @chat = Chat.find(params[:id])
+  end
+  
+  def gets_entries_all_messages
+    @chats = @room.chats.includes(:user).order("created_at asc")
+    @entries = @room.user_rooms
+  end
+  
+  def chat_params
+    params.require(:chat).permit(:user_id, :chat, :room_id).merge(user_id: current_user.id)
+  end
+  
 end
