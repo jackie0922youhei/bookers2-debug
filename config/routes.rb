cac21293f89915bb64b1e746fb229703165c136a
jackie0922youhei <jackie0922youhei@gmail.com>
2020-11-07 00:22:09 +0000

    create controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'chats/create'
+  get 'chats/show'
+  get 'rooms/create'
   root to: 'homes#top'
 
   get 'home/about' => 'homes#about', as: 'about'
@@ -21,6 +24,8 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy] # :book_id :id　＝＞　/books/:book_id/book_comments/:id
   end
-
+  
+  resources :rooms, only: [:create]
+  resources :chats, only: [:create, :show]
 
 end
