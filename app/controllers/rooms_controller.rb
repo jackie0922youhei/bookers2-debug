07ad17c18501eda514a3e28de3d049fe57227f12
jackie0922youhei <jackie0922youhei@gmail.com>
2020-11-07 02:23:45 +0000

    rooms controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,17 @@
 class RoomsController < ApplicationController
+  
+  before_action :authenticate_user!
+  
   def create
+    @room = Room.create
+    @joinCurrentUser = UserRoom.create(user_id: current_user.id, room_id: @room.id)
+    @joinUser = UserRoom.create(join_room_params)
+    redirect_to chats_path
   end
+  
+  private
+  def join_room_params
+    params.require(:user_room).permit(:user_id, :room_id).merge(room_id: @room.id)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,14 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   get 'home/about' => 'homes#about', as: 'about'
-  
+
   get 'search' => 'searches#search'
 
   devise_for :users, :controllers => {
     :sessions => 'users/sessions',
     :registrations => 'users/registrations'
   }
-  
+
   resources :users, only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
     get :follows, on: :member
@@ -24,8 +24,8 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy] # :book_id :id　＝＞　/books/:book_id/book_comments/:id
   end
-  
+
   resources :rooms, only: [:create]
-  resources :chats, only: [:create, :show]
+  resource :chats, only: [:create, :show]
 
 end
